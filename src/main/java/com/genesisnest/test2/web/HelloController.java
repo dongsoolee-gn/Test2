74644be64205c11a7fe3ade457d45cdace8b022a
dongsoolee-gn <dongsoo.lee@genesisnest.com>
2022-07-17 20:36:45 +0900

    testd

--- src/main/java/com/genesisnest/test2/web/HelloController.java
+++ src/main/java/com/genesisnest/test2/web/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello. tesdsftnewfddfsd1";
+        return "Hello. tesdsftnddfsd1";
     }
 
 
